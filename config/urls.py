dd3f5b2f48865431beca2eaa0cccfb5796b8f682
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-08-13 18:34:48 +0200

    fix static files

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import user_passes_test
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("harvest.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+else: urlpatterns += [ re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})]
