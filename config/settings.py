0f3346c4cbe29f65ac6869b1d5822490c0ad1ce0
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-08-13 14:47:11 +0200

    add admins.py

--- config/settings.py
+++ config/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 }
+CSRF_TRUSTED_ORIGINS = ['*']
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Password validation
