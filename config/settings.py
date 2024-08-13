246c9f2e1c0cf377b4a90ea5d0acb7ea83ae05d2
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-08-13 13:40:39 +0200

    commit

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default':  dj_database_url.parse(DATABASE_URL, conn_max_age=600),
+    'default':  dj_database_url.config(default=os.environ.get('DATABASE_URL')),
     #   {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
