1eed8b054fe49dfccf9d5e6d35120e23236887d9
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-08-14 13:49:09 +0200

    fix: scope for manifest and service worker

--- harvest/urls.py
+++ harvest/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', view=views.home),
     path("complete-payment-form/", view=views.complete_payment_form, name="card-complete-payment-form"),
     path('shop/',views.shop, name="shop"),
     path("blog/",views.blog,name="blog"),
-    path("single_product/",views.single_product,name="single_product")
+    path("single_product/",views.single_product,name="single_product"), 
+    path('manifest.json', TemplateView.as_view(template_name='PWA/manifest.json', content_type='application/json')),
+    path('service-worker.js', TemplateView.as_view(template_name='PWA/service-worker.js', content_type='application/javascript')),
 ]
