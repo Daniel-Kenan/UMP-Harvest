8be32050767812c4c2c5e297dd7c8f816e5a1610
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-08-20 17:20:25 +0200

    changed redirect url for google auth on deployment

--- config/settings.py
+++ config/settings.py
@@ -80,12 +80,15 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 if not DEBUG:
     MIDDLEWARE.append('allauth.account.middleware.AccountMiddleware')
+else: 
 
 
 import os
